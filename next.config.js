0d8cc034ee4443ddd0384500dad0a6ce4823c382
DhanyaBhatt <67457850+Dhanya3301@users.noreply.github.com>
2024-04-26 11:45:37 +0530

    finalest commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["localhost", "shik-shak-shop-production.up.railway.app"],
+  },
+};
+
+module.exports = nextConfig;
